f3398956bc9bb8fe7d9c88bf1586613b4cc07621
syedmuheed <syedmuheed@devsed.com>
2024-05-31 18:27:23 +0530

    kk

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" min-h-[110vh] bg-cover bg-[url('/images/resort-in-jodhpur.jpg')] w-full">
-      <div className=" text-black  text-[3.4rem] w-[47rem] absolute flex top-[42%] left-[50%] -translate-x-1/2 -translate-y-1/2 font-scr font-bold">Your One stop for your Special Days</div>
+    <div className=" min-h-[110vh] bg-cover bg-[url('/images/background/loginbg.jpg')] w-full">
+      <div className=" text-[#050505]  text-[3.4rem] text-center w-[35rem] absolute 
+      flex top-[42%] left-[50%] -translate-x-1/2 -translate-y-1/2 font-gel 
+      font-bold">Your One stop for your Special Days</div>
     </div>
   );
 }
