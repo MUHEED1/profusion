48f8ac97ac687d110a4e401157621e16b89f5b3c
Mujahed <khanmujahed8444@gmail.com>
2024-05-31 16:58:08 +0530

    kk

---
+++ app/components/padding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Padding = ({children,className}) => {
+  return (
+    <div className={`px-[1rem] md:px-[2rem] lg:px-[3rem] ${className}`}>{children}</div>
+  )
+}
+
+export default Padding
--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" min-h-[120vh] bg-cover bg-[url('/images/resort-in-jodhpur.jpg')] w-full">
-      <div></div>
+    <div className=" min-h-[110vh] bg-cover bg-[url('/images/resort-in-jodhpur.jpg')] w-full">
+      <div className="text-[#00000]  text-[3.5rem] absolute flex top-[42%] left-[50%] -translate-x-1/2 -translate-y-1/2 font-scr font-bold">Your One stop for your Special Days</div>
     </div>
   );
 }
