461a28e3d9c8f331fad82426446a25311d618de3
Mujahed <khanmujahed8444@gmail.com>
2024-05-31 20:20:40 +0530

    kk

--- app/page.js
+++ app/page.js
@@ -7,6 +7,30 @@ export default function Home() {
       <div className=" text-pink-600  leading-[7rem] text-[3.5rem] text-center w-[35rem] absolute 
       flex top-[40%] left-[50%] -translate-x-1/2 -translate-y-1/2 font-scr 
       font-bold">Your One stop for your Special Days</div>
+      <div>
+      Studio S Weddingz
+      WEDNARA
+      Meragi Photography
+      RVR PRO
+      More Than Red
+      Blink Event Photographers
+      Flash Fusion Studios
+      </div>
+      <div className="pt-[10rem] font-semibold">
+      <marquee >
+      <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Catering</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Venue</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Cars & Travelling</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Dresses</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Invitation card</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Decoration</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Events</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Baraat</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Photography</div>
+    <div class="inline-block font-gel text-[2rem] text-pink-600 px-5">Jewelry</div>
+</marquee>
+
+      </div>
     </div>
   );
 }
