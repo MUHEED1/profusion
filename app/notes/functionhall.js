d1c750c6dcc287538e07425db59d8619a11fe45a
Mahfooz1416 <abdulmahfooz@devsed.com>
2024-05-31 23:36:43 +0530

    ll

---
+++ app/notes/functionhall.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Functionhall = () => {
+  return (
+    <div>
+        Kings Kohinoor Convention
+N Convention
+Subham Convention Centre
+JRC Conventions
+The Vintage Palace
+Krystal Gardens
+Red Rose Palace
+Raj Mahal Function Hall
+Mallika Convention
+City Convention Center
+H F Convention
+SS Grand Convention
+Hamara Family Garden Hall
+Nr Convention Hall
+City Convention Center
+Down Town Club Halls
+Mughals Banquet Hall
+Paradise Garden
+The Vintage Palace
+S V Convention
+Pvr Convention Centre
+Central Court Convention Hall
+Chowmahalla Palace
+NKNR Gardens
+Metro Classic Garden
+Snr Convention
+Nr Convention
+Ashoka Function Palace
+Elite Convention Hall
+Pride O S Function Hall
+Taj Convention
+Rhodium 7 Convention Hall
+Park Exotica Malang
+Central Court Convention Hall
+F M Pride Function Hall
+    </div>
+  )
+}
+
+export default Functionhall
---
+++ app/notes/makeup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Makeup = () => {
+  return (
+    <div>
+        1. HD Makeup
+2. Airbrush Makeup
+3. Matte Makeup Look
+4. Mineral Makeup Look
+5. Natural Minimal Makeup Look
+6. Smokey Eye Bridal Makeup
+7. Nude Bridal Makeup
+8. Hijab bridal makeup
+    </div>
+  )
+}
+
+export default Makeup
