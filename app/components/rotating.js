09d757c1fd4aaa8977b68a548383909c30c28331
Mahfooz1416 <abdulmahfooz@devsed.com>
2024-06-09 19:14:20 +0530

    kk

--- app/components/rotating.js
+++ app/components/rotating.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
 import Circle from "./circle";
 import Page99 from "./page99";
@@ -15,11 +15,29 @@ const Rotating = () => {
     };
   }, [scrollYProgress]);
 
-  const t = useTransform(scrollYProgress, [0.1, 0.5], ["0rem", "0rem"]);
+  const t = useTransform(scrollYProgress, [0.1, 1], ["0rem", "0rem"]);
 
-  const pd = useTransform(scrollYProgress, [0, 0.74], ["39%", "6%"]);
+  const pd = useTransform(scrollYProgress, [0, 0.74], ["35%", "6%"]);
+
+  const [scrollY, setScrollY] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="bg-[#101010]">
+    <div
+      className={`  ${
+        scrollY > 4500 ? "bg-white" : "bg-[#101010]"
+      } duration-1000`}
+    >
       <div className="  relative">
         <motion.div
           className={` hidden lg:block sticky z-10`}
@@ -28,7 +46,7 @@ const Rotating = () => {
           {/* ${ scrollY>9500 && scrollY < 12500 ? " sticky ":" relative"} */}
           <div ref={maf} className=" overflow-hidden  pt-[10rem]">
             <motion.div
-              className=" flex  w-full   h-[110vh]  relative z-[1000]"
+              className=" flex  w-full   h-[80vh]  relative z-[1000]"
               style={{ translateX: pd }}
             >
               <div className="   ">
